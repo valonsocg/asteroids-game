c64205d60d5f8cf4ef73a2d0fe7526c0e98b37d1
valonsocg <valonsocg@gmail.com>
2026-01-12 22:24:17 -0500

    Refactor main function to move initialization logs before pygame setup

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from logger import log_state
 
 
 def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -11,9 +14,7 @@ def main():
         log_state()
         screen.fill("black")
         pygame.display.flip()
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    
 
 
 if __name__ == "__main__":
